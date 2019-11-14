3d892e87da8fe97f7da8f0950707a3c155915292
Azorlax <davidmsmiley@gmail.com>
2019-11-14 18:06:34 -0500

    added text to file

---
+++ 99_bottles_of_beer.py
@@ -0,0 +1,15 @@
+# project to write the song 99 bottles of beer on the wall
+# cannot use numbers in song, except "take 1 down"
+for i in range(99, 0, -1):
+    if i > 1:
+        print(f'\n\n{i} bottles of beer on the wall. \n{i} bottles of beer.')
+        print("Take one down, pass it around...")
+        i -= 1
+        if i > 1:
+            print(f'{i} bottles of beer on the wall.')
+        else:
+            print(f'{i} bottle of beer on the wall.')
+    else:
+        print(f"\n\n{i} bottle of beer on the wall. {i} bottle of beer.")
+        print("Take one down, pass it around...")
+        print("No more bottles of beer on the wall.")
